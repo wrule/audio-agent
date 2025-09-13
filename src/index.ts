5ac9e3c73664721fa201c29df65d97886b0a83e6
36000.eth <1982775886@qq.com>
2025-09-13 22:22:02 +0800

    feat: base feat

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
+import applescript from 'applescript';
 
-console.log(1234);
+// 设置系统音量 (0-100)
+function setVolume(volume: number) {
+  const script = `set volume output volume ${volume}`;
+  applescript.execString(script, (err, result) => {
+    if (err) console.error(err);
+    else console.log('音量已设置为:', volume);
+  });
+}
+
+setVolume(40);
